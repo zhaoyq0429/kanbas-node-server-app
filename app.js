67acbbc721ff5957cc3b5fec10e371fb4449148a
zhaoyq0429 <zhaoyq0429@gmail.com>
2023-12-03 14:05:32 -0800

    change mongoose

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ import cors from "cors";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
-
-
 const app = express();
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 
 app.use(
   cors({
