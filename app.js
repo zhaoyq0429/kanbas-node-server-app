5baa5dac8137ce53a892c4a450c74c2f296ae5d7
zhaoyq0429 <zhaoyq0429@gmail.com>
2023-12-02 21:05:38 -0800

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
-// import session from "express-session";
+import session from "express-session";
 import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
@@ -7,6 +7,11 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentsRoutes from "./assignments/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
 
 const app = express();
 
@@ -19,7 +24,22 @@ app.use(
         : process.env.FRONTEND_URL_LOCAL,
   })
 );
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentsRoutes(app);
---
+++ users/routes.js
@@ -0,0 +1,70 @@
+import * as dao from "./dao.js";
+
+function UserRoutes(app) {
+  const createUser = async (req, res) => {
+    const user = await dao.createUser(req.body);
+    res.json(user);
+  };
+  const deleteUser = async (req, res) => {
+    const status = await dao.deleteUser(req.params.userId);
+    res.json(status);
+  };
+  const findAllUsers = async (req, res) => {
+    const users = await dao.findAllUsers();
+    res.json(users);
+  };
+  const findUserById = async (req, res) => {
+    const id = req.params.id;
+    const user = await dao.findUserById(id);
+    res.json(user);
+  };
+  const findUserByUsername = async (req, res) => {
+    const username = req.params.username;
+    const user = await dao.findUserByUsername(username);
+    res.json(user);
+  };
+  const updateUser = async (req, res) => {
+    const id = req.params.id;
+    const status = await dao.updateUser(id, req.body);
+    const currentUser = await dao.findUserById(id);
+    console.log(req.session);
+    req.session['currentUser'] = currentUser; 
+    res.json(status);
+  };
+  const signup = async (req, res) => {
+    const user = await dao.findUserByUsername(req.body.username);
+    if (user) {
+      res.status(400).json({ message: "Username already taken" });
+    }
+    const currentUser = await dao.createUser(req.body);
+    req.session['currentUser'] = currentUser;
+    res.json(currentUser);
+  };
+
+  const signin = async (req, res) => {
+    const { username, password } = req.body;
+    const currentUser = await dao.findUserByCredentials(username, password);
+    req.session['currentUser'] = currentUser;
+    res.json(currentUser);
+  };
+  const signout = (req, res) => {
+    req.session.destroy();
+    res.json(200);
+  };
+
+  const account = async (req, res) => {
+    res.json(req.session['currentUser']);
+  };
+
+  app.get("/api/users", findAllUsers);
+  app.get("/api/users/:id", findUserById);
+  app.get("/api/users/username/:username", findUserByUsername);
+  app.put("/api/users/:id", updateUser);
+  app.delete("/api/users/:userId", deleteUser);
+  app.post("/api/users", createUser);
+  app.post("/api/users/signup", signup);
+  app.post("/api/users/signin", signin);
+  app.post("/api/users/signout", signout);
+  app.post("/api/users/account", account);
+}
+export default UserRoutes;
